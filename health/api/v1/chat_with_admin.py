f7b9a04d8beaae0b34e0938a2b16d24e7d8552af
Lingg26 <nttl261101@gmail.com>
2024-01-08 00:26:44 +0700

    feat(api): create api statistics

--- health/api/v1/chat_with_admin.py
+++ health/api/v1/chat_with_admin.py
@@ -1,7 +1,7 @@
 import json
 
 from fastapi import APIRouter, Depends, HTTPException, status, Request
-from sqlalchemy import func
+from sqlalchemy import func, and_
 from sqlalchemy.orm import Session
 
 from health import models
@@ -12,6 +12,67 @@ from health.shared.core_type import UserType
 from health.tools.deps import get_current_authenticated_user, get_database_session
 
 router = APIRouter()
+
+@router.get(
+    "/users",
+    summary="Get list user"
+)
+async def get_list_user_chat(
+        db: Session = Depends(get_database_session),
+        current_user: models.Account = Depends(get_current_authenticated_user)
+):
+    if current_user.user_type == UserType.ADMIN:
+        sub_query = (
+            db.query(
+                models.ChatHistory.account_id.label("account_id"), func.max(models.ChatHistory.id).label("id")
+            )
+            .group_by(models.ChatHistory.account_id)
+            .subquery()
+        )
+        accounts = (
+            db.query(
+                *models.Account.__table__.columns,
+                func.json_object(
+                    'id',
+                    models.ChatHistory.id,
+                    'created_at',
+                    models.ChatHistory.created_at,
+                    'sent_account_id',
+                    models.ChatHistory.sent_account_id,
+                    'account_id',
+                    models.ChatHistory.account_id,
+                    'status',
+                    models.ChatHistory.status
+                ).label('chat')
+            )
+            .outerjoin(models.ChatHistory, models.ChatHistory.account_id == models.Account.id)
+            .join(sub_query, and_(sub_query.c.account_id == models.Account.id, sub_query.c.id == models.ChatHistory.id))
+            .all()
+        )
+        response = []
+        for account in accounts:
+            data_copy = dict(account)
+            if data_copy['chat']:
+                chat = json.loads(data_copy['chat'])
+            else:
+                chat = {}
+            if chat and chat['sent_account_id'] == chat['account_id'] and chat['status'] == 'sent':
+                data_copy['is_read'] = False
+            else:
+                data_copy['is_read'] = True
+            data_copy["chat_id"] = chat["id"]
+            data_copy.pop('chat')
+            response.append(data_copy)
+
+        return {
+            'data': response
+        }
+    else:
+        raise HTTPException(
+            status.HTTP_403_FORBIDDEN,
+            detail="You don't have permission"
+        )
+
 
 
 @router.get(
@@ -65,63 +126,4 @@ async def send_message(
     )
     return message
 
-
-@router.get(
-    "/get_user",
-    summary="Get list user"
-)
-async def get_list_user(
-        db: Session = Depends(get_database_session),
-        current_user: models.Account = Depends(get_current_authenticated_user)
-):
-    if current_user.user_type == UserType.ADMIN:
-        sub_query = (
-            db.query(
-                models.ChatHistory.account_id, func.max(models.ChatHistory.id)
-            )
-            .group_by(models.ChatHistory.account_id)
-            .sub
-        )
-        accounts = (
-            db.query(
-                *models.Account.__table__.columns,
-                func.json_object(
-                    'id',
-                    models.ChatHistory.id,
-                    'created_at',
-                    models.ChatHistory.created_at,
-                    'sent_account_id',
-                    models.ChatHistory.sent_account_id,
-                    'account_id',
-                    models.ChatHistory.account_id,
-                    'status',
-                    models.ChatHistory.status
-                ).label('chat')
-            )
-            .outerjoin(models.ChatHistory, models.ChatHistory.account_id == models.Account.id)
-            .join(sub_query, sub_query.account_id == models.Account.id)
-            .all()
-        )
-        response = []
-        for account in accounts:
-            data_copy = dict(account)
-            if data_copy['chat']:
-                chat = json.loads(data_copy['chat'])
-            else:
-                chat = {}
-            if chat and chat['sent_account_id'] == chat['account_id'] and chat['status'] == 'sent':
-                data_copy['is_read'] = False
-            else:
-                data_copy['is_read'] = True
-            data_copy.pop('chat')
-            response.append(data_copy)
-
-        return {
-            'data': response
-        }
-    else:
-        raise HTTPException(
-            status.HTTP_403_FORBIDDEN,
-            detail="You don't have permission"
-        )
 
